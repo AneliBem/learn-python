85607144df8ff75c0ee41f6f04d3804d75bb02e7
AneliBem <tolya.bem@gmail.com>
2024-12-04 17:40:41 +0200

    first commit

---
+++ variables-data-4.py
@@ -0,0 +1,22 @@
+num = 15
+print(num)
+
+del num
+
+num = 6
+text = "value"
+print(text, num)
+
+booll = True
+print(num + booll)
+
+
+
+a = int(input("Enter number 1: "))
+b = input("Enter number 2: ")
+
+a += 5 #скороченні дії
+b *= 3
+
+print(a + float(b))
+
