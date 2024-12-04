ecfd23ffb74eae15ce2103ef1102275942297d24
AneliBem <tolya.bem@gmail.com>
2024-12-04 17:28:56 +0200

    first commit

---
+++ basic-action-3.py
@@ -0,0 +1,14 @@
+print("hello word", 98, 59, 9.8, True, sep="...", end="?")
+print("\n\tworld")
+print('hello')
+print('\\john\'s')
+print("john\"s")
+
+print(7 + 6)
+print(min(5, 6, 8, 1))
+print(max(5, 6, 8, 1))
+print(pow(4, 8))
+print(round(5.56))
+
+
+input("Print number: ")
